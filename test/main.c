281f0ea521c05bf9eb30d95fb7c98fa7929e129c
Olebol - CODAM <ole.pelser@gmail.com>
2023-05-02 23:14:40 +0200

    updated to make it clean | THERE ARE LEAKS

--- test/main.c
+++ test/main.c
@@ -6,52 +6,91 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/02 16:47:12 by opelser       #+#    #+#                 */
-/*   Updated: 2023/05/02 21:36:07 by opelser       ########   odam.nl         */
+/*   Updated: 2023/05/02 23:12:55 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/include/libft.h"
 
-int		main(int argc, char **argv, char **envp)
+char	*parse(char **envp)
 {
-	char	**split;
-	char	*path;
+	int		i;
+
+	i = 0;
+	while (ft_strncmp("PATH=", envp[i], 5))
+		i++;
+	return (ft_substr(envp[i], 5, ft_strlen(envp[i]) - 5));
+}
+
+void	free_ptr_arr(void **ptrs)
+{
+	int		i;
+
+	i = 0;
+	while (ptrs[i])
+	{
+		free(ptrs[i]);
+		i++;
+	}
+	free(ptrs);
+}
+
+char	*get_path(char **paths)
+{
+	int		i;
+
+	i = 0;
+	while (paths[i])
+	{
+		if (access(paths[i], F_OK) == 0)
+			return (paths[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+// dont return NULL for malloc errors?
+char	*get_command_location(char *command, char **paths)
+{
+	int		i;
+
+	i = 0;
+	command = ft_strjoin("/", command);
+	if (!command)
+		return (NULL);
+	while (paths[i])
+	{
+		paths[i] = ft_strjoin_free(paths[i], command);
+		if (!paths[i])
+		{
+			free_ptr_arr((void **) paths);
+			return (NULL);
+		}
+		i++;
+	}
+	return (get_path(paths));
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	**split_paths;
+	char	*paths;
 	char	*command;
-	char	*correct_path;
-	
+
 	(void) argc;
 	command = argv[1];
 
-	while (ft_strncmp("PATH=", *envp, 5))
-		envp++;
-	// printf("%s\n\n", *envp);
-
-	path = ft_split(*envp, '=')[1];
-	if (!path)
+	paths = parse(envp);
+	if (!paths)
 		return (1);
-	// printf("%s\n\n", path);
 
-	split = ft_split(path, ':');
-	if (!split)
-		return (1);
-	// for (int i = 0; split[i]; i++)
-	// 	printf("%s\n", split[i]);
-	// putchar('\n');
+	split_paths = ft_split(paths, ':');
+	if (!split_paths)
+		return (2);
 
-	command = ft_strjoin("/", command);
-	
-	for (int i = 0; split[i]; i++)
-		split[i] = ft_strjoin_free(split[i], command);
-	// for (int i = 0; split[i]; i++)
-	// 	printf("%s\n", split[i]);
-	// putchar('\n');
+	command = get_command_location(argv[1], split_paths);
+	if (!command)
+		return (3);
 
-	for (int i = 0; split[i]; i++)
-	{
-		if (access(split[i], F_OK) == 0)
-			correct_path = split[i];
-	}
-	// printf("%s\n", correct_path);
-
-	execv(correct_path, argv + 1);
+	execve(command, NULL, envp);
 }
