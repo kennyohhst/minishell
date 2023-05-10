0fd84f18f206ed9ebf629caddd724962e5a948fd
Olebol - CODAM <ole.pelser@gmail.com>
2023-05-10 16:54:41 +0200

    functioning echo

--- src/builtins.c
+++ src/builtins.c
@@ -6,11 +6,27 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/08 21:08:18 by opelser       #+#    #+#                 */
-/*   Updated: 2023/05/08 21:18:57 by opelser       ########   odam.nl         */
+/*   Updated: 2023/05/10 16:53:58 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	check_options(char *options)
+{
+	int		i;
+
+	i = 1;
+	if (options[0] != '-')
+		return (0);
+	while (options[i])
+	{
+		if (options[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	echo(char **argv)
 {
@@ -24,7 +40,7 @@ int	echo(char **argv)
 		write(1, "\n", 1);
 		return (1);
 	}
-	if (!ft_strncmp("-n", argv[1], 3))
+	if (check_options(argv[1]))
 	{
 		newline = '\0';
 		i++;
@@ -36,6 +52,6 @@ int	echo(char **argv)
 			write(1, " ", 1);
 		i++;
 	}
-	write(1, "\n", 1);
+	write(1, &newline, 1);
 	return (1);
 }
