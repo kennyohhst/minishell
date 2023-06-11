2fff01937e3e9d54bdd9a24a1de4fedba13d5f69
Olebol - CODAM <ole.pelser@gmail.com>
2023-06-11 16:41:44 +0200

    modular argv's

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/30 21:40:35 by opelser       #+#    #+#                 */
-/*   Updated: 2023/06/11 13:58:37 by opelser       ########   odam.nl         */
+/*   Updated: 2023/06/11 16:38:44 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-int	orig;
-
-void	close_if_valid(int fd)
+void	close_pipe(int fd_in, int fd_out)
 {
-	if (fd >= 0)
-		close(fd);
+	if (fd_in >= 0)
+		close(fd_in);
+	if (fd_out >= 0)
+		close(fd_out);
 }
 
 void	execute_command(char **argv, int fd_in, int fd_out)
@@ -45,9 +45,9 @@ void	execute_command(char **argv, int fd_in, int fd_out)
 		printf("execve error\n");
 		exit(1);
 	}
-	waitpid(pid, &status, 0);
 	close_if_valid(fd_in);
 	close_if_valid(fd_out);
+	waitpid(pid, &status, 0);
 	printf("exit status: %d\n\n", status);
 }
 
@@ -56,23 +56,24 @@ int		main(void)
 	char	*argv1[3] = {"/bin/cat", "test.c", NULL};
 	char	*argv2[3] = {"/usr/bin/wc", "-l", NULL};
 	char	*argv3[3] = {"/bin/cat", "-e", NULL};
+	char	**argvs[4] = {argv1, argv2, argv3, NULL};
 	int		fd[2];
 	int		fd2[2];
 	int		pid;
-
-	orig = dup(1);
+	int		i = 1;
 
 	if (pipe(fd) == -1)
 		exit(1);
 	if (pipe(fd2) == -1)
 		exit(1);
 
-	execute_command(argv1, -1, fd[1]);
-	execute_command(argv2, fd[0], fd2[1]);
-	execute_command(argv3, fd2[0], -1);
-
-	dup2(orig, 1);
-	close(orig);
+	execute_command(argvs[0], -1, fd[1]);
+	while(argvs[i + 1])
+	{
+		execute_command(argvs[i], fd[0], fd2[1]);
+		i++;
+	}
+	execute_command(argvs[i], fd2[0], -1);
 
 	return (69);
 }
