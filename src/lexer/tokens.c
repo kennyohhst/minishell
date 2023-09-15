3b565eccc9dc1312f8d254326e2ddf956d64872f
Kenny <kennyohhst@gmail.com>
2023-09-15 16:20:47 +0200

    quotes should work n'o'w''

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -6,11 +6,35 @@
 /*   By: kkalika <kkalika@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 14:10:55 by kkalika           #+#    #+#             */
-/*   Updated: 2023/09/10 16:46:14 by kkalika          ###   ########.fr       */
+/*   Updated: 2023/09/15 16:18:56 by kkalika          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	quotes_finder(char *str, char c)
+{
+	int	i;
+	int	count_q;
+
+	count_q = 0;
+	i = 0;
+	while (str[i] && str[i] != ' ')
+	{
+		if (str[i] == c || (count_q % 2))
+		{
+			while (str[i] && str[i] != c)
+				i++;
+			count_q++;
+		}
+		i++;
+	}
+	while (str[i] && str[i] != ' ')
+		i++;
+	if (count_q == 2)
+		i++;
+	return (i - count_q);
+}
 
 int	remove_quotes(char *str, char c)
 {
@@ -35,6 +59,33 @@ int	remove_quotes(char *str, char c)
 		i++;
 	}
 	return (i);
+}
+
+int	std_remove_quotes(char *str, char c)
+{
+	int	i;
+	int	x;
+	int	len;
+
+	i = 0;
+	x = 0;
+	len = quotes_finder(str, c);
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+		{
+			while (str[i] != '\0')
+			{
+				ft_memset(str + i, str[i + 1], 1);
+				i++;
+				x++;
+			}
+			i = i - x - 1;
+			x = 0;
+		}
+		i++;
+	}
+	return (i - len);
 }
 
 int	p_d_token(t_input **cmd, char *str, int i, char c)
@@ -86,7 +137,7 @@ int	d_quotes_token(t_input **cmd, char *str, t_data data)
 	if ((str[i] == '\0' && str[i - 1] == '\"') || quote == true)
 	{
 		i = remove_quotes(str, '\"');
-		return (add_nodes(cmd, NULL, ft_substr(str, 0, i), DQ_STRING), (i + 1));
+		return (add_nodes(cmd, NULL, ft_substr(str, 0, i+1), DQ_STRING), (i + 1));
 	}
 	return (-1);
 }
@@ -118,8 +169,8 @@ int	std_token(t_input **cmd, char *str)
 			return (add_nodes(cmd, NULL, ft_substr(str, 0, i), STRING), i);
 		else if (str[i] == '\"' || str[i] == '\'')
 		{
-			i = remove_quotes(str, str[i]);
-			if (!str[i])
+			i = std_remove_quotes(str, str[i]);
+			if (!str[i] || str[i] == ' ')
 				return (add_nodes(cmd, NULL, ft_substr(str, 0, i), STRING), i);
 			else
 				i--;
