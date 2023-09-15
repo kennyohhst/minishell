9154e5609e973099136f61ccde9cc0f2d8cffa92
Kenny <kennyohhst@gmail.com>
2023-09-15 17:40:26 +0200

    expand_fix

--- src/parser/expander.c
+++ src/parser/expander.c
@@ -6,7 +6,7 @@
 /*   By: kkalika <kkalika@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 20:27:18 by kkalika           #+#    #+#             */
-/*   Updated: 2023/09/02 15:15:50 by kkalika          ###   ########.fr       */
+/*   Updated: 2023/09/15 17:28:40 by kkalika          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ char	*replace_env_with_value(char *token, char *e_var, int start, int end)
 	char	*temp_s;
 	char	*temp_e;
 
+	if (start == -1)
+		return (token);
 	if (!e_var)
 	{
 		temp_s = ft_substr(token, 0, (start));
@@ -64,6 +66,25 @@ int	skip_singles(int i, char *token)
 	return (i);
 }
 
+int	find_start_exit_var(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i = skip_singles(i, str);
+		if (str[i] == '$')
+		{
+			i++;
+			if (str[i] == '?')
+				return (i-1);
+		}
+		i++;
+	}
+	return (-1);
+}
+
 bool	expander(t_data data, char **token, char *temp, int i)
 {
 	int		end;
@@ -73,20 +94,21 @@ bool	expander(t_data data, char **token, char *temp, int i)
 		i = skip_singles(i, (*token));
 		if ((*token)[i] == '$')
 		{
-			if (ft_strnstr((*token), "$?", ft_strlen((*token))))
-			{
-				temp = ft_itoa(data.exit_code);
-				(*token) = replace_env_with_value((*token), temp, i, 2);
-			}
-			else if (expand_edgecases((*token)[i + 1]))
+			if (expand_edgecases((*token)[i + 1]))
 			{
 				end = end_position((*token) + i);
 				temp = ft_getenv(data.envp, 
-						ft_substr((*token), i + 1, end - 1));
+				ft_substr((*token), i + 1, end - 1));
 				(*token) = replace_env_with_value((*token), temp, i, end);
 			}
 		}
 		i++;
+	}
+	if (ft_strnstr((*token), "$?", ft_strlen((*token))))
+	{
+		i = find_start_exit_var(*token);
+		temp = ft_itoa(data.exit_code);
+		(*token) = replace_env_with_value((*token), temp, i, 2);
 	}
 	return (false);
 }
